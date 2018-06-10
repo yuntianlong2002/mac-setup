58c778061eb65a9054cd0ff815801614646d7f20
yuntianlong2002 <yuntianlong2002@gmail.com>
2018-06-10 16:58:42 -0700

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -4,3 +4,7 @@ wget --no-check-certificate -O /usr/local/bin/bd https://raw.github.com/vigneshw
 chmod +rx /usr/local/bin/bd
 echo 'alias bd=". bd -si"' >> ~/.bashrc
 source ~/.bashrc
+brew install googler
+
+npm install fanyi -g
+pip install gcalcli
